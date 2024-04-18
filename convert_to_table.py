bdbccf04652a587fc3a0bc7707d1900d5958d3a0
Qiao Zhang <qiaozhd2@gmail.com>
2024-04-17 22:19:46 -0400

    write to md

--- convert_to_table.py
+++ convert_to_table.py
@@ -12,25 +12,31 @@ def convert(game_scores):
     table_header = list(game_scores[0].keys())
     col_width = max(len(col) for col in game_scores[0]) + 5
 
-    rows.append('|'.join(s.ljust(col_width) for s in table_header))
-    rows.append('|'.join('-' * col_width for _ in range(len(table_header))))
+    rows.append('|'+'|'.join(s.ljust(col_width) for s in table_header)+'|')
+    rows.append('|'+'|'.join('-' * col_width for _ in range(len(table_header)))+'|')
     for score in game_scores:
-        rows.append('|'.join(str(v).ljust(col_width) for k, v in score.items() if k in table_header))
-    for row in rows:
-        print(f'|{row}|')
+        rows.append('|'+'|'.join(str(v).ljust(col_width) for k, v in score.items() if k in table_header)+'|')
+
+    # for row in rows:
+    #     print(f'|{row}|')
     return rows
 
-# def write_md(results, f):
-#     for row in results:
-#         # f.write(f'')
-#         f.write(f"| {row[0]} | {row[1]} | {row[2]} |\n")
+the_same_part = """---
+title: Hackathon Leaderboard
+nav_title: Leaderboard
+permalink: /leaderboard/
+layout: page
+order: 5
+---
 
+"""
 if __name__ == "__main__":
     with open('files/game_scores.json', 'r') as f:
         game_scores = json.load(f)
     game_score_md = convert(game_scores)
-    print(game_score_md)
+    # print(game_score_md)
 
-    # with open('leaderboard.markdown', 'w') as f:
-    #     write_md(game_score_md, 'test.markdown')
-
+    with open('leaderboard.markdown', 'w') as f:        
+        f.write(the_same_part)       
+        for row in game_score_md:
+            f.write(row + '\n')
